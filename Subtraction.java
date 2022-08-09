bd20bede6bf3e605ed7146f3e46aab95c4026d72
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-09 18:04:10 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+class Subtraction{
+public static void main(String[]args){
+System.out.println("the two number is");
+int a =25;
+int b = 15;
+
+System.out.println(a + " " + b);
+int sub = a-b;
+
+System.out.println("the sub is:"+ sub);
+}
+} 
