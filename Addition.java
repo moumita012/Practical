9e2ee29259ef29f91309dce97a2de97ea51c1cdc
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-09 17:42:41 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Addition{
+public static void main(String[]args){
+System.out.println("the two number is;");
+int a = 3; //initialize 1st local variable
+int b = 10;//initialize 2nd local variable
+
+System.out.println(a + " " + b);
+int sum = a+b;//logic
+
+System.out.println("the sum is:"+ sum);
+}
+} 
---
+++ Division.java
@@ -0,0 +1,12 @@
+class Division{
+public static void main(String[]args){
+System.out.println("the two number is;");
+int a =100;//initialize 1st local variable
+int b = 10;//initialize 2nd local variable
+
+System.out.println(a + " " + b);
+int divi = a/b; // logic
+
+System.out.println("the  divi is:"+divi);
+}
+} 
