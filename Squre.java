776fd39e7ffc7b0add59319cd60f6666dac5a425
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-10 15:48:33 +0530

    Delete Squre.java

--- Squre.java
+++
@@ -1,9 +0,0 @@
-class Squre{
-public static void main(String[]arg){
-System.out.println(" Enter side of a squre");
-int side= 5;
-int area= side*side;
-int perimeter= 4*side;
-System.out.println(" the area is"+area);
-System.out.println(" the perimeter is"+ perimeter);
-}}
