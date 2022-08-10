c21ad40a3ad6f7275e8bb02f2177dab5ad2d214e
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-10 16:03:54 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+public static void main(String[]arg){
+System.out.println(" The side of a  Rectangular");
+int length= 5;
+int breadth=4;
+System.out.println("the length is"+length );
+System.out.println("the breadth is"+breadth);
+int area=  length* breadth;
+int perimeter=2*( length+breadth);
+System.out.println(" the area is"+area);
+System.out.println(" the perimeter is"+ perimeter);
+}}
