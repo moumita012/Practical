42939a5a7baac1ddce32476dbbb1e6414eea6e10
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-16 17:33:46 +0530

    Add files via upload

---
+++ Operatorexample.java
@@ -0,0 +1,33 @@
+package Operator;
+
+public class Operatorexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 35;
+		int b= 20;
+		int c= 20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(a<b&&b<a);
+		System.out.println(a>b&&b<a); 
+		System.out.println(a<b||b<a); 
+		System.out.println(b==c);
+		
+		System.out.println(a++ + ++a);
+		System.out.println(b-- + --b);
+		
+		System.out.println(a<b);
+         System.out.println(a>b);
+         
+		
+		
+		
+
+	}
+
+}
